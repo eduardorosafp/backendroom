d58281b9b865a10aba6b1fbc1210ae677fc4ae3f
eduardorosafp <eduardorosafp@gmail.com>
2024-02-18 16:04:57 -0300

    instanciando o banco de dados db.jsonfile

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,21 +1,33 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database/database.sqlite",
-});
+const jsonfile = require("jsonfile");
+const file = "db.json";
 
-const Task = sequelize.define("Task", {
-  description: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
+const Task = {
+  create: (description) => {
+    jsonfile.readFile(file, (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      data.tasks.push({ description });
+      jsonfile.writeFile(file, data, { spaces: 2 }, (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    });
   },
-});
-
-(async () => {
-  await sequelize.sync();
-})();
+  findAll: () => {
+    return new Promise((resolve, reject) => {
+      jsonfile.readFile(file, (err, data) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(data.tasks);
+      });
+    });
+  },
+};
 
 module.exports = Task;
+
